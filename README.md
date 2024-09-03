51a0699ae6c1608d4ed102f329ebc4696a511958
Gugustinette <mercier.augustin@outlook.fr>
2024-09-03 17:50:49 +0200

    feat: init starter without debug tools

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # starter-3d
 A 3d game starter based on Vite
+
+## Usage
+
+- Clone the repository
+
+```bash
+npx degit fibbojs/starter-3d my-fibbo-game
+```
+
+- Install the dependencies
+
+```bash
+cd my-fibbo-game
+npm install
+```
+
+- Start the development server
+
+```bash
+npm run dev
+```
---
+++ src/main.ts
@@ -0,0 +1,61 @@
+import * as THREE from 'three'
+import RAPIER from '@dimforge/rapier3d'
+import { FComponentEmpty, FCuboid, FGameCamera, FScene } from '@fibbojs/3d'
+import { FKeyboard } from '@fibbojs/event'
+// import { fDebug } from '@fibbojs/devtools'
+import './style.css'
+import Character from './classes/Character'
+
+(async () => {
+  // Initialize the scene
+  const scene = new FScene()
+  scene.init()
+  await scene.initPhysics()
+  // Debug the scene
+  /*
+  if (import.meta.env.DEV)
+    fDebug(scene)
+  */
+
+  // Add ambient light
+  const light = new THREE.AmbientLight(0xFFFFFF)
+  scene.scene.add(light)
+
+  // Create a death zone
+  const deathZone = new FComponentEmpty(scene, {
+    position: { x: 0, y: -20, z: 0 },
+    scale: { x: 100, y: 1, z: 100 },
+  })
+  deathZone.initCollider()
+  scene.addComponent(deathZone)
+
+  // Create a ground
+  const ground = new FCuboid(scene, {
+    position: { x: 0, y: -0.1, z: 0 },
+    scale: { x: 15, y: 0.1, z: 15 },
+  })
+  ground.initRigidBody({
+    rigidBodyType: RAPIER.RigidBodyType.Fixed,
+  })
+  ground.setColor(0x348C31)
+  scene.addComponent(ground)
+
+  // Create a character
+  const character = new Character(scene)
+  scene.addComponent(character)
+
+  // Attach a camera to the character
+  scene.camera = new FGameCamera({ target: character })
+
+  // Add collision events
+  character.onCollisionWith(deathZone, () => {
+    console.log('Character fell into the death zone!')
+    character.setPosition({ x: 0, y: 10, z: 0 })
+  })
+
+  // Create keyboard
+  const keyboard = new FKeyboard(scene)
+  keyboard.onKeyDown('p', () => {
+    character.setPosition({ x: 0, y: 5, z: 0 })
+  })
+})()
